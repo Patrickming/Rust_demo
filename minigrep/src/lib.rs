5cd3729b59275ad7032b22d09691f789be1d88fa
Patrickming <patrickmingx@gmail.com>
2024-02-21 21:01:39 +0800

    test-minigrep3

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -18,10 +18,43 @@ impl Config {
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     //当我们把访问一个没有权限的文件报错：Should have been able to read the file: Os { code: 5, kind: PermissionDenied, message: "拒绝访问。" }
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    println!("With text:\n{contents}");
+    let contents = fs::read_to_string(config.file_path)?;
+    for line in search(&config.query, &contents) {
+        print!("{line}")
+    }
     Ok(())
 }
+
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    //这里的 lines 返回一个迭代器
+    for line in contents.lines() {
+        //Rust 的字符串还提供了 contains 方法，用于检查 line 是否包含待查询的 query。
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        // 测试用例将在指定的内容中搜索 duct 字符串
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
