f47b8ee23f1256f091c37cb128d0c092780ac4d9
Patrickming <patrickmingx@gmail.com>
2024-02-22 09:51:41 +0800

    eprint - minigrep5

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,6 @@
+use minigrep::Config;
 use std::env;
 use std::process;
-use minigrep::Config;
 fn main() {
     //读取并分析传入的命令行参数，然后通过 collect 方法输出一个集合类型 Vector。
     let args: Vec<String> = env::args().collect();
@@ -10,16 +10,16 @@ fn main() {
     /*如果 Result 是 Ok，那该方法就类似 unwrap：返回 Ok 内部的值；
     如果是 Err，就调用闭包中的自定义代码对错误进行进一步处理 */
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         //不再panic!让程序崩溃，而是终结进程
         process::exit(1); //非0数
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.file_path);
 
-   if let Err(e) = minigrep::run(config){
-       println!("Application error: {e}");
-       process::exit(1);
-   } ;
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    };
 }
